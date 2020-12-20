af41fbdc02293c96372b81ae9c9762c449de9340
Shah Zaib Ali Dogar <shahzaibali008@hotmail.com>
2020-12-20 17:13:16 +0500

    Update decorators.py

--- BackEndApp/decorators.py
+++ BackEndApp/decorators.py
@@ -17,18 +17,14 @@ def unauthenticated_user(view_func):
 
 def allowed_users(allowed=[]):
     def decorator(view_func):
-        print('Started')
 
         def wrapper_func(request, *args, **kwargs):
-            print('In There')
             group = None
             if request.user.groups.exists():
                 group = request.user.groups.all()[0].name
-                print(group)
                 if group == None:
                     return HttpResponse('Page access denied!!!')
                 if group in allowed:
-                    print(group)
                     return view_func(request, *args, **kwargs)
                 else:
                     return HttpResponse('Page access denied!!!')
