927d986ec1b067d5c0ee4c7597b4c5e2125a3f53
Usama Rizwan <uvirk447@gmail.com>
2021-05-06 00:29:33 +0500

    Baseline GUI of timeline

--- BackEndApp/urls.py
+++ BackEndApp/urls.py
@@ -11,6 +11,11 @@ urlpatterns = [
     path('addLabReport/', views.addLabReport, name='addLabReport'),
     path('profile/', views.profile, name='profile'),
     path('summary/', views.summary, name='summary'),
+
+
+    path('timeLine/', views.timeLine, name='timeLine'),
+    
+    
     path('viewAllRecords/', views.viewAllRecords, name='viewAllRecords'),
     #path('viewReport/', views.viewReport, name='viewReport'),
     path('reset_password/',
