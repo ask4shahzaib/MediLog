47364b1eb2b89e7860b6b42af02ff35666c59131
Usama Rizwan <uvirk447@gmail.com>
2021-05-06 23:15:23 +0500

    Updated patient profile sidebar with null mesg in timeline template

--- static/js/index.js
+++ static/js/index.js
@@ -16,3 +16,20 @@ function stringlength(inputtxt, minlength, maxlength) {
     return true;
   }
 }
+
+// For form validation
+function validateForm(){
+	var cnic = document.forms["form1"]["your-CNIC"].value;
+	var phone = document.forms["form1"]["your-phone"].value;
+
+	if (!(cnic.length==13)) {
+        document.getElementById('cnic').innerHTML = "CNIC must be 13 digits long *"
+    }
+    if (phone.length<1) {
+        document.getElementById('error-phone').innerHTML = " Please Enter Your Phone *";      
+    }
+      
+    if(phone.length<1 || cnic.length<1){
+       	return false;
+    }            
+}
